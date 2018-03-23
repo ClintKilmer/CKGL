5c21b9719726ad1ed40f0be24f1bf40af44cb490
Clint Kilmer <gamedev@clintkilmer.net>
2018-03-23 15:48:51 -0600

    working

---
+++ CKGLTest/Shaders/Test.glsl
@@ -0,0 +1,18 @@
+﻿#version 330 core
+uniform vec2 offset;
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec4 colour;
+out vec4 vertexColour;
+void main()
+{
+	vertexColour = colour;
+	gl_Position = vec4(position.x + offset.x, position.y + offset.y, position.z, 1.0);
+}
+...
+#version 330 core
+in vec4 vertexColour;
+out vec4 color;
+void main()
+{
+	color = vertexColour;
+}
--- CKGLTest/src/Program.cs
+++ CKGLTest/src/Program.cs
@@ -39,26 +39,7 @@ namespace CKGLTest
 			GL.BufferData(BufferTarget.ElementArray, sizeof(GLuint) * indices.Length, indices, BufferUsage.StaticDraw);
 
 			// Shader - create/compile/link/set program
-			Shader shader = new Shader(@"
-				#version 330 core
-				uniform vec2 offset;
-				layout(location = 0) in vec3 position;
-				layout(location = 1) in vec4 colour;
-				out vec4 vertexColour;
-				void main()
-				{
-					vertexColour = colour;
-					gl_Position = vec4(position.x + offset.x, position.y + offset.y, position.z, 1.0);
-				}
-				...
-				#version 330 core
-				in vec4 vertexColour;
-				out vec4 color;
-				void main()
-				{
-					color = vertexColour;
-				}
-			");
+			Shader shader = Shader.FromFile("Shaders/test.glsl");
 			GL.UseProgram(shader.id);
 			GLint offset = GL.GetUniformLocation(shader.id, "offset");
 
