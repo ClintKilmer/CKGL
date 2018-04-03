f96bc2a59f98ba0ea0033f7e5f618ceccb78f64b
ClintKilmer <gamedev@clintkilmer.net>
2018-04-02 23:35:59 -0600

    Started Texture abstraction

---
+++ CKGL/src/Graphics/Texture.cs
@@ -0,0 +1,67 @@
+﻿using static  SDL2.SDL_image;
+
+using OpenGL;
+
+using GLint = System.Int32;
+using GLuint = System.UInt32;
+
+namespace CKGL
+{
+	public class Texture
+	{
+		private static GLuint currentlyBoundTexture;
+
+		private GLuint ID;
+
+		public string File { get; private set; } = "";
+		public int Width { get; private set; } = 0;
+		public int Height { get; private set; } = 0;
+		public int BitsPerPixel { get; private set; } = 0;
+
+		private GLuint localBuffer = 0;
+
+		public Texture()
+		{
+			Bind();
+		}
+
+		private void Generate()
+		{
+			if (ID == 0)
+				ID = GL.GenTexture();
+		}
+
+		public void Destroy()
+		{
+			if (ID != 0)
+			{
+				GL.DeleteTexture(ID);
+				ID = 0;
+			}
+		}
+
+		public void Bind()
+		{
+			Generate();
+
+			if (ID != currentlyBoundTexture)
+			{
+				GL.BindTexture(TextureTarget.Texture2D, ID);
+				currentlyBoundTexture = ID;
+			}
+		}
+
+		//public void UnBind()
+		//{
+		//	GL.BindTexture(TextureTarget.Texture2D, 0);
+		//	currentlyBoundTexture = 0;
+		//}
+
+		//public void LoadImage()
+		//{
+		//	Bind();
+		//	GL.BufferData(BufferTarget.ElementArray, sizeof(GLint) * indices.Length, indices, bufferUsage);
+		//	Count = indices.Length;
+		//}
+	}
+}
