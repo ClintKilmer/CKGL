ee946943c8d6e5b3ae59a055fa9cbc268335fc14
ClintKilmer <gamedev@clintkilmer.net>
2018-04-05 03:18:02 -0600

    Added AABB collision classes

---
+++ CKGL/src/Collision/AABB.cs
@@ -0,0 +1,49 @@
+﻿namespace CKGL
+{
+	public class AABB
+	{
+		public Vector2 Position { get; set; } = Vector2.Zero;
+		public float X
+		{
+			get { return Position.X; }
+			set { Position = new Vector2(value, Y); }
+		}
+		public float Y
+		{
+			get { return Position.Y; }
+			set { Position = new Vector2(X, value); }
+		}
+
+		public Vector2 Size { get; set; } = Vector2.Zero;
+		public float Width
+		{
+			get { return Size.X; }
+			set { Size = new Vector2(value, Height); }
+		}
+		public float Height
+		{
+			get { return Size.Y; }
+			set { Size = new Vector2(Width, value); }
+		}
+
+		public float Top { get { return Position.Y; } }
+		public float Bottom { get { return Position.Y + Size.Y; } }
+		public float Left { get { return Position.X; } }
+		public float Right { get { return Position.X + Size.X; } }
+
+		public AABB(float x, float y, float width, float height) : this(new Vector2(x, y), new Vector2(width, height)) { }
+		public AABB(Vector2 position, Vector2 size)
+		{
+			Position = position;
+			Size = size;
+		}
+
+		public bool Intersects(AABB aabb)
+		{
+			return Right > aabb.Left &&
+				   Left < aabb.Right &&
+				   Bottom > aabb.Top &&
+				   Top < aabb.Bottom;
+		}
+	}
+}
---
+++ CKGL/src/Collision/AABBi.cs
@@ -0,0 +1,31 @@
+﻿namespace CKGL
+{
+	public class AABBi
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int Width { get; set; }
+		public int Height { get; set; }
+
+		public int Top { get { return Y; } }
+		public int Bottom { get { return Y + Height; } }
+		public int Left { get { return X; } }
+		public int Right { get { return X + Width; } }
+
+		public AABBi(int x, int y, int width, int height)
+		{
+			X = x;
+			Y = y;
+			Width = width;
+			Height = height;
+		}
+
+		public bool Intersects(AABBi aabbi)
+		{
+			return Right > aabbi.Left &&
+				   Left < aabbi.Right &&
+				   Bottom > aabbi.Top &&
+				   Top < aabbi.Bottom;
+		}
+	}
+}
