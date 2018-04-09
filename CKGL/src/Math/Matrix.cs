5a85f64c37a6df0bdc0a8aebdab2f6e2d3fa5282
ClintKilmer <gamedev@clintkilmer.net>
2018-04-09 16:03:35 -0600

    Enable Quaternion math in Matrix.cs

--- CKGL/src/Math/Matrix.cs
+++ CKGL/src/Math/Matrix.cs
@@ -309,51 +309,46 @@ namespace CKGL
 			return result;
 		}
 
-		// TODO - Quaternion
-		//public static Matrix CreateFromQuaternion(ref Quaternion quaternion)
-		//{
-		//	Matrix result = Identity;
+		public static Matrix CreateFromQuaternion(Quaternion quaternion)
+		{
+			Matrix result = Identity;
 
-		//	float num9 = quaternion.X * quaternion.X;
-		//	float num8 = quaternion.Y * quaternion.Y;
-		//	float num7 = quaternion.Z * quaternion.Z;
-		//	float num6 = quaternion.X * quaternion.Y;
-		//	float num5 = quaternion.Z * quaternion.W;
-		//	float num4 = quaternion.Z * quaternion.X;
-		//	float num3 = quaternion.Y * quaternion.W;
-		//	float num2 = quaternion.Y * quaternion.Z;
-		//	float num = quaternion.X * quaternion.W;
-		//	result.M11 = 1f - (2f * (num8 + num7));
-		//	result.M12 = 2f * (num6 + num5);
-		//	result.M13 = 2f * (num4 - num3);
-		//	result.M14 = 0f;
-		//	result.M21 = 2f * (num6 - num5);
-		//	result.M22 = 1f - (2f * (num7 + num9));
-		//	result.M23 = 2f * (num2 + num);
-		//	result.M24 = 0f;
-		//	result.M31 = 2f * (num4 + num3);
-		//	result.M32 = 2f * (num2 - num);
-		//	result.M33 = 1f - (2f * (num8 + num9));
-		//	result.M34 = 0f;
-		//	result.M41 = 0f;
-		//	result.M42 = 0f;
-		//	result.M43 = 0f;
-		//	result.M44 = 1f;
+			float num9 = quaternion.X * quaternion.X;
+			float num8 = quaternion.Y * quaternion.Y;
+			float num7 = quaternion.Z * quaternion.Z;
+			float num6 = quaternion.X * quaternion.Y;
+			float num5 = quaternion.Z * quaternion.W;
+			float num4 = quaternion.Z * quaternion.X;
+			float num3 = quaternion.Y * quaternion.W;
+			float num2 = quaternion.Y * quaternion.Z;
+			float num = quaternion.X * quaternion.W;
+			result.M11 = 1f - (2f * (num8 + num7));
+			result.M12 = 2f * (num6 + num5);
+			result.M13 = 2f * (num4 - num3);
+			result.M14 = 0f;
+			result.M21 = 2f * (num6 - num5);
+			result.M22 = 1f - (2f * (num7 + num9));
+			result.M23 = 2f * (num2 + num);
+			result.M24 = 0f;
+			result.M31 = 2f * (num4 + num3);
+			result.M32 = 2f * (num2 - num);
+			result.M33 = 1f - (2f * (num8 + num9));
+			result.M34 = 0f;
+			result.M41 = 0f;
+			result.M42 = 0f;
+			result.M43 = 0f;
+			result.M44 = 1f;
 
-		//	return result;
-		//}
+			return result;
+		}
 
-		// TODO - Quaternion
-		//public static Matrix CreateFromYawPitchRoll(float yaw, float pitch, float roll)
-		//{
-		//	Matrix result = Identity;
+		public static Matrix CreateFromYawPitchRoll(float yaw, float pitch, float roll)
+		{
+			Matrix result = Identity;
 
-		//	Quaternion quaternion;
-		//	Quaternion.CreateFromYawPitchRoll(yaw, pitch, roll, out quaternion);
-		//	CreateFromQuaternion(ref quaternion, out result);
-
-		//	return result;
-		//}
+			Quaternion quaternion = Quaternion.CreateFromYawPitchRoll(yaw, pitch, roll);
+			return CreateFromQuaternion(quaternion);
+		}
 
 		public static Matrix CreateLookAt(Vector3 cameraPosition, Vector3 cameraTarget, Vector3 cameraUpVector)
 		{
