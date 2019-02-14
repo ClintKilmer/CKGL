ebaee4f340dceaad29d4d2e0a43c208b10da228c
ClintKilmer <gamedev@clintkilmer.net>
2019-02-14 10:05:01 -0700

    Cleanup RenderTarget Enums

--- CKGL/src/Graphics/RenderTarget.cs
+++ CKGL/src/Graphics/RenderTarget.cs
@@ -5,6 +5,29 @@ using GLuint = System.UInt32;
 
 namespace CKGL
 {
+	#region Enums
+	public enum TextureSlot : int
+	{
+		Depth = -1,
+		Colour0 = 0,
+		Colour1 = 1,
+		Colour2 = 2,
+		Colour3 = 3,
+		Colour4 = 4,
+		Colour5 = 5,
+		Colour6 = 6,
+		Colour7 = 7,
+		Colour8 = 8,
+		Colour9 = 9,
+		Colour10 = 10,
+		Colour11 = 11,
+		Colour12 = 12,
+		Colour13 = 13,
+		Colour14 = 14,
+		Colour15 = 15
+	}
+	#endregion
+
 	public class RenderTarget
 	{
 		public static readonly RenderTarget Default = new RenderTarget();
