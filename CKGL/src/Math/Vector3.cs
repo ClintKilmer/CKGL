051628f3163ef6971b65d138cc389cbbc0d7f3d3
ClintKilmer <gamedev@clintkilmer.net>
2019-05-29 19:47:24 -0600

    Vector3 optimizations

--- CKGL/src/Math/Vector3.cs
+++ CKGL/src/Math/Vector3.cs
@@ -259,18 +259,15 @@ namespace CKGL
 		// Pre-multiplication only
 		public static Vector3 operator *(Vector3 v, Matrix m)
 		{
-			var x = (v.X * m.M11) + (v.Y * m.M21) + (v.Z * m.M31) + m.M41;
-			var y = (v.X * m.M12) + (v.Y * m.M22) + (v.Z * m.M32) + m.M42;
-			var z = (v.X * m.M13) + (v.Y * m.M23) + (v.Z * m.M33) + m.M43;
-			return new Vector3(x, y, z);
+			return new Vector3(
+				(v.X * m.M11) + (v.Y * m.M21) + (v.Z * m.M31) + m.M41,
+				(v.X * m.M12) + (v.Y * m.M22) + (v.Z * m.M32) + m.M42,
+				(v.X * m.M13) + (v.Y * m.M23) + (v.Z * m.M33) + m.M43);
 		}
 		// Pre-multiplication conversion from Post-multiplication order
 		public static Vector3 operator *(Matrix m, Vector3 v)
 		{
-			var x = (v.X * m.M11) + (v.Y * m.M21) + (v.Z * m.M31) + m.M41;
-			var y = (v.X * m.M12) + (v.Y * m.M22) + (v.Z * m.M32) + m.M42;
-			var z = (v.X * m.M13) + (v.Y * m.M23) + (v.Z * m.M33) + m.M43;
-			return new Vector3(x, y, z);
+			return v * m;
 		}
 		// Pre-multiplication only
 		public static Vector3 operator *(Vector3 v, Quaternion q)
