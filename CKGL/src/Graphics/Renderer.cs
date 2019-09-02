45a24ab8d5b1cb653abbabf1a1e1322e47d10d6d
ClintKilmer <gamedev@clintkilmer.net>
2019-09-01 23:30:42 -0600

    Consolidated Renderer (api-agnostic)

--- CKGL/src/Graphics/Renderer.cs
+++ CKGL/src/Graphics/Renderer.cs
@@ -1,47 +1,141 @@
 using System;
+using System.Runtime.InteropServices;
 
 namespace CKGL
 {
 	public static class Renderer
 	{
-		private static RendererBase renderer;
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		internal struct Vertex
+		{
+			internal Vector3 Position;
+			internal Colour Colour;
+			internal UV UV;
+			internal byte Textured;
+
+			internal Vertex(Vector3 position, Colour colour, UV uv, bool textured)
+			{
+				Position = position;
+				Colour = colour;
+				UV = uv;
+				Textured = (byte)(textured ? 255 : 0);
+			}
+
+			internal Vertex(Vector3 position, Colour? colour, UV? uv)
+				: this(position, colour ?? Colour.White, uv ?? UV.Zero, uv != null) { }
+		}
+
+		private static VertexBuffer vertexBuffer;
+		private static VertexFormat vertexFormat;
+		private static GeometryInput geometryInput;
+		private static PrimitiveTopology currentPrimitiveTopology = PrimitiveTopology.TriangleList;
+		// Adaptive vertex buffer size
+		//private static int bufferSize = 16384; // Performant on Laptop
+		private static int bufferSize = 1024;
+		private static readonly int maxBufferSize = int.MaxValue;
+		//private const int bufferSize = 1998; // Divisible by 3 and 2 for no vertex wrapping per batch
+		private static Vertex[] vertices;
+		private static int vertexCount = 0;
 
 		internal static void Init()
 		{
-			switch (Platform.GraphicsBackend)
-			{
-#if VULKAN
-				case GraphicsBackend.Vulkan:
-					throw new NotImplementedException("Vulkan Backend not implemented yet.");
-#endif
-#if OPENGL
-				case GraphicsBackend.OpenGL:
-#endif
-#if OPENGLES
-				case GraphicsBackend.OpenGLES:
-#endif
-#if OPENGL || OPENGLES
-					renderer = new OpenGL.OpenGLRenderer();
-					break;
-#endif
-#if WEBGL
-				case GraphicsBackend.WebGL:
-					renderer = new WebGL.WebGLRenderer();
-					break;
-#endif
-				default:
-					throw new NotSupportedException($"GraphicsBackend {Platform.GraphicsBackend} not supported.");
-			}
+			vertexBuffer = VertexBuffer.Create(BufferUsage.Dynamic);
+			vertexFormat = new VertexFormat(
+				//Marshal.SizeOf(typeof(Vertex)),                       // Dynamic Stride - For larger StructLayout Pack sizes
+				new VertexAttribute(DataType.Float, 3, false),        // Position
+				new VertexAttribute(DataType.UnsignedByte, 4, true),  // Colour
+				new VertexAttribute(DataType.UnsignedShort, 2, true), // UV
+				new VertexAttribute(DataType.UnsignedByte, 1, true)   // Textured
+			);
+			geometryInput = GeometryInput.Create(new VertexStream(vertexBuffer, vertexFormat));
 
-			renderer.Init();
+			vertices = new Vertex[bufferSize];
+
+			Graphics.State.OnStateChanging += Flush;
+
+			// Debug
+			Output.WriteLine($"Renderer Initialized");
 		}
 
-		internal static void Destroy() => renderer.Destroy();
+		internal static void Destroy()
+		{
+			geometryInput.Destroy();
+			vertexBuffer.Destroy();
 
-		public static void Flush() => renderer.Flush();
+			geometryInput = null;
+			vertexBuffer = null;
 
-		private static void AddVertex(PrimitiveTopology primitiveTopology, Vector3 position, Colour? colour, UV? uv)
-			=> renderer.AddVertex(primitiveTopology, position, colour, uv);
+			Graphics.State.OnStateChanging -= Flush;
+		}
+
+		public static void Flush()
+		{
+			if (
+				(currentPrimitiveTopology == PrimitiveTopology.TriangleList && vertexCount >= 3) ||
+				(currentPrimitiveTopology == PrimitiveTopology.TriangleStrip && vertexCount >= 3) ||
+				(currentPrimitiveTopology == PrimitiveTopology.TriangleFan && vertexCount >= 3) ||
+				(currentPrimitiveTopology == PrimitiveTopology.LineList && vertexCount >= 2) ||
+				(currentPrimitiveTopology == PrimitiveTopology.LineStrip && vertexCount >= 2) ||
+				(currentPrimitiveTopology == PrimitiveTopology.LineLoop && vertexCount >= 2) ||
+				(currentPrimitiveTopology == PrimitiveTopology.PointList && vertexCount >= 1)
+			)
+			{
+				geometryInput.Bind();
+				vertexBuffer.LoadData(vertices);
+				Graphics.DrawVertexArrays(currentPrimitiveTopology, 0, vertexCount);
+			}
+
+			// Reset vertexCount so we don't lose any vertex data
+			int remainder = 0;
+			switch (currentPrimitiveTopology)
+			{
+				case (PrimitiveTopology.TriangleList):
+					remainder = vertexCount % 3;
+					break;
+				case (PrimitiveTopology.LineList):
+					remainder = vertexCount % 2;
+					break;
+				default:
+					remainder = 0;
+					break;
+			}
+			for (int i = 0; i < remainder; i++)
+			{
+				vertices[i] = vertices[vertexCount - remainder + i];
+			}
+			vertexCount = remainder;
+		}
+
+		internal static void AddVertex(PrimitiveTopology type, Vector3 position, Colour? colour, UV? uv)
+		{
+			if (currentPrimitiveTopology != type)
+			{
+				Flush();
+				currentPrimitiveTopology = type;
+				// We can lose vertices here, but it's ok as we're switching primitive types anyways
+				vertexCount = 0;
+			}
+
+			if (vertexCount >= bufferSize)
+			{
+				Flush();
+
+				// Adaptive vertex buffer size
+				if (bufferSize < maxBufferSize)
+				{
+					bufferSize *= 2;
+					if (bufferSize > maxBufferSize)
+						bufferSize = maxBufferSize;
+
+					Array.Resize(ref vertices, bufferSize);
+
+					Output.WriteLine($"Renderer - VertexBuffer size: {bufferSize:n0}");
+				}
+			}
+
+			vertices[vertexCount] = new Vertex(position, colour, uv);
+			vertexCount++;
+		}
 
 		#region Draw
 		public static class Draw
